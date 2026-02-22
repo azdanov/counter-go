4ff7c322d23f18f05036c19eb6bb1b4864123ccb
Anton Ždanov <anton@azdanov.dev>
2026-02-22 10:49:23 +0200

    fix: correctly count words

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func main() {
 
 func countWords(bytes []byte) int {
 	wordCount := 0
-
+	inWord := false
 	for _, b := range bytes {
 		if b == ' ' || b == '\n' {
+			inWord = false
+		} else if !inWord {
+			inWord = true
 			wordCount++
 		}
 	}
-
 	return wordCount
 }
