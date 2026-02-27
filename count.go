7f7f451084a8af2839a61948d87026d73c70b74d
Anton Ždanov <anton@azdanov.dev>
2026-02-27 15:37:05 +0200

    fix: resolve issue with piping stdin

--- count.go
+++ count.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 	"log"
+	"unicode"
 )
 
 type Counts struct {
@@ -21,59 +22,32 @@ func (c Counts) Add(o Counts) Counts {
 }
 
 func Count(f io.ReadSeeker) Counts {
-	lines := CountLines(f)
-	f.Seek(0, io.SeekStart)
-	words := CountWords(f)
-	f.Seek(0, io.SeekStart)
-	bytes := CountBytes(f)
+	c := Counts{}
 
-	return Counts{
-		Lines: lines,
-		Words: words,
-		Bytes: bytes,
-	}
-}
+	r := bufio.NewReader(f)
+	inWord := false
 
-func CountLines(r io.Reader) int {
-	reader := bufio.NewReader(r)
-
-	count := 0
 	for {
-		r, _, err := reader.ReadRune()
+		r, size, err := r.ReadRune()
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			log.Fatalln("Error reading file:", err)
 		}
+
 		if r == '\n' {
-			count++
+			c.Lines++
 		}
+
+		isSpace := unicode.IsSpace(r)
+		if !isSpace && !inWord {
+			c.Words++
+		}
+		inWord = !isSpace
+
+		c.Bytes += size
 	}
 
-	return count
-}
-
-func CountWords(r io.Reader) int {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
-
-	count := 0
-	for scanner.Scan() {
-		count++
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatalln("Error scanning file:", err)
-	}
-
-	return count
-}
-
-func CountBytes(r io.Reader) int {
-	count, err := io.Copy(io.Discard, r)
-	if err != nil {
-		log.Fatalln("Error counting bytes:", err)
-	}
-	return int(count)
+	return c
 }
--- count_test.go
+++ count_test.go
@@ -8,143 +8,6 @@ import (
 	counter "github.com/azdanov/counter-go"
 )
 
-func Test_countWords(t *testing.T) {
-	tests := []struct {
-		name string
-		r    io.Reader
-		want int
-	}{
-		{
-			name: "empty input",
-			r:    strings.NewReader(""),
-			want: 0,
-		},
-		{
-			name: "single space",
-			r:    strings.NewReader(" "),
-			want: 0,
-		},
-		{
-			name: "single word",
-			r:    strings.NewReader("one"),
-			want: 1,
-		},
-		{
-			name: "multiple words",
-			r:    strings.NewReader("one two three four five"),
-			want: 5,
-		},
-		{
-			name: "words with newlines",
-			r:    strings.NewReader("one\n\ntwo three\nfour\nfive"),
-			want: 5,
-		},
-		{
-			name: "words with multiple spaces",
-			r:    strings.NewReader("one   two    three     four      five"),
-			want: 5,
-		},
-		{
-			name: "words with tabs and newlines",
-			r:    strings.NewReader("one\t\two\nthree\tfour\n\nfive"),
-			want: 5,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := counter.CountWords(tt.r)
-			if got != tt.want {
-				t.Errorf("countWords() = %d, want %d", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCountLines(t *testing.T) {
-	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
-		r    io.Reader
-		want int
-	}{
-		{
-			name: "empty input",
-			r:    strings.NewReader(""),
-			want: 0,
-		},
-		{
-			name: "single line without newline",
-			r:    strings.NewReader("one line"),
-			want: 0,
-		},
-		{
-			name: "multiple lines",
-			r:    strings.NewReader("line one\nline two\nline three\nline four\nline five"),
-			want: 4,
-		},
-		{
-			name: "lines with empty lines",
-			r:    strings.NewReader("line one\n\nline two\n\nline three\n\nline four\n\nline five"),
-			want: 8,
-		},
-		{
-			name: "lines with trailing newline",
-			r:    strings.NewReader("line one\nline two\nline three\nline four\nline five\n"),
-			want: 5,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := counter.CountLines(tt.r)
-			if got != tt.want {
-				t.Errorf("CountLines() = %d, want %d", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCountBytes(t *testing.T) {
-	tests := []struct {
-		name string
-		r    io.Reader
-		want int
-	}{
-		{
-			name: "empty input",
-			r:    strings.NewReader(""),
-			want: 0,
-		},
-		{
-			name: "single character",
-			r:    strings.NewReader("a"),
-			want: 1,
-		},
-		{
-			name: "multiple characters",
-			r:    strings.NewReader("hello world"),
-			want: 11,
-		},
-		{
-			name: "string with newlines",
-			r:    strings.NewReader("hello world\nhello world\nhello world"),
-			want: 35,
-		},
-		{
-			name: "string with unicode characters",
-			r:    strings.NewReader("привет мир"),
-			want: 19, // 18 bytes for the characters + 1 byte for the space
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := counter.CountBytes(tt.r)
-			if got != tt.want {
-				t.Errorf("CountBytes() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestCount(t *testing.T) {
 	tests := []struct {
 		name string
@@ -165,6 +28,11 @@ func TestCount(t *testing.T) {
 			name: "multiple lines with words",
 			f:    strings.NewReader("hello world\nhello world\nhello world"),
 			want: counter.Counts{Lines: 2, Words: 6, Bytes: 35},
+		},
+		{
+			name: "string with tabs and multiple spaces",
+			f:    strings.NewReader("hello\tworld  \nhello\tworld"),
+			want: counter.Counts{Lines: 1, Words: 4, Bytes: 25},
 		},
 		{
 			name: "string with unicode characters",
