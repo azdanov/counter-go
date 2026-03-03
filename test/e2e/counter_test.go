6a5d0e2cead1a85ab1d7fb11a990ed28802a9c96
Anton Ždanov <anton@azdanov.dev>
2026-03-03 11:55:25 +0200

    test: add flag tests

--- test/e2e/counter_test.go
+++ test/e2e/counter_test.go
@@ -3,6 +3,8 @@ package e2e
 import (
 	"bytes"
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -34,6 +36,7 @@ func TestSingleFile(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer os.Remove(file.Name())
 
 	cmd, err := getCmd(file.Name())
 	if err != nil {
@@ -86,5 +89,48 @@ func TestNonExistentFile(t *testing.T) {
 	wantErr := fmt.Sprintf("%s: open %s: no such file or directory\n", binName, fileName)
 	if !bytes.Contains(errBuf.Bytes(), []byte(wantErr)) {
 		t.Fatalf("unexpected: got %q, want it to contain %q", errBuf.String(), wantErr)
+	}
+}
+
+func TestFlags(t *testing.T) {
+	file, err := createFile("Hello, World!\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	tests := []struct {
+		name     string
+		flag     string
+		expected string
+	}{
+		{"lines", "-l", fmt.Sprintf(" 1 %s\n", file.Name())},
+		{"bytes", "-c", fmt.Sprintf(" 14 %s\n", file.Name())},
+		{"words", "-w", fmt.Sprintf(" 2 %s\n", file.Name())},
+		{"headers", "-headers", fmt.Sprintf(" lines words bytes\n     1     2    14 %s\n", file.Name())},
+		{"all flags", "-l -w -c -headers", fmt.Sprintf(" lines words bytes\n     1     2    14 %s\n", file.Name())},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			args := append(strings.Fields(tt.flag), file.Name())
+			cmd, err := getCmd(args...)
+			if err != nil {
+				t.Fatal("couldn't create command:", err)
+			}
+
+			outBuf := &bytes.Buffer{}
+			errBuf := &bytes.Buffer{}
+			cmd.Stdout = outBuf
+			cmd.Stderr = errBuf
+
+			if err := cmd.Run(); err != nil {
+				t.Fatalf("failed to run the test binary: %s: %s", err, errBuf.String())
+			}
+
+			if outBuf.String() != tt.expected {
+				t.Fatalf("unexpected: got %q, want %q", outBuf.String(), tt.expected)
+			}
+		})
 	}
 }
